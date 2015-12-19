c29262f990ee829d4723310db2771a75a6c9d23d
zf2timo <tewes@henworx.de>
2015-12-19 22:30:02 +0100

    Removed Test Class

--- tests/PrinterTest.php
+++ tests/PrinterTest.php
@@ -10,5 +10,9 @@ class PrinterTest extends \PHPUnit_Framework_TestCase
     public function testTestCaseNameIsDisplayed()
     {
 
+        $mock = $this->getMock('\PHPUnit_Framework_Test', null, array(), 'Foo\Bar\CaseTest');
+
+        $printer = new Printer();
+        $printer->startTest($mock);
     }
 } 
